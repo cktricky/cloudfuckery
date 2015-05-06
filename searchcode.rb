7dd3f2b7b996331e8e18447395fcea39562a7cda
cktricky <cktricky@gmail.com>
2015-05-06 19:22:38 -0400

    making some decent progress I guess

--- searchcode.rb
+++ searchcode.rb
@@ -1,4 +1,7 @@
 require 'optparse'
+require 'json'
+require 'open-uri'
+
 
 Options = Struct.new(:name)
 
@@ -24,9 +27,68 @@ class Parser
   end
 end
 
+class PrintTable
+  
+  
+  def initialize(hash={})
+    length_info = get_length_values(hash)
+    make_titles(length_info)
+  end
+  
+  def get_length_values(hash={})
+    key_lengths = []
+    val_lengths = []
+    hash.each do |key, value|
+      key_lengths << key.to_s.length
+      val_lengths << value.to_s.length
+    end
+    { :key_length => key_lengths.sort_by(&:to_i).last, 
+      :value_length => val_lengths.sort_by(&:to_i).last
+    }
+  end
+  
+  def make_titles(length_info={})
+    title = ""
+    title << "Category\n" 
+    title << "=" * 8
+    title << ' ' * (length_info[:key_length] - 8 + 1)
+    puts title.inspect
+  end
+  
+end
+
+class GetOrgMembers
+  
+  attr_reader :name
+  
+  def initialize(options=Struct.new)
+    @name = options.name
+  end
+  
+  def fetch
+    @members = JSON.parse(open("https://api.github.com/orgs/#{name}/members").read)
+  rescue OpenURI::HTTPError => e
+    e
+  end
+  
+  def print_relevant_details
+    if @members.nil?
+     puts "No members Returned, Sorry"
+     exit
+    end
+    @members.each do |member|
+      PrintTable.new(member)
+    end
+  end
+
+end
 
 args = ARGV.empty? ? %w{--help} : ARGV
 options = Parser.parse args
 p options if args.include?('--help')
+
+members = GetOrgMembers.new(options)
+members.fetch
+members.print_relevant_details
 
 
